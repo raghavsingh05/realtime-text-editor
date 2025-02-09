6cca70f306a3406b754581f5c34feed88f2e54fc
raghavsingh05 <raghavsingh47336@gmail.com>
2025-02-09 17:33:56 +0530

    Add header component and authentication pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,10 @@
 import Header from '@/components/Header'
-import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      home
+      <Header />
     </div>
   )
 }
