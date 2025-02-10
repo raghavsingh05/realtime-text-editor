29404c0ecdea3f811860d4c6e3468c64638220fb
raghavsingh05 <raghavsingh47336@gmail.com>
2025-02-10 10:55:52 +0530

    Add collaborative document features and user authentication handling

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,47 @@
 import Header from '@/components/Header'
+import AddDocumentBtn from '@/components/ui/AddDocumentBtn'
+import { Button } from '@/components/ui/button'
+import { SignedIn, UserButton } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs/server'
+import Image from 'next/image'
 import React from 'react'
 
-const page = () => {
+const Home = async () => {
+  const clerkUser = await currentUser();
+  if (!clerkUser) {
+    return <div>Loading...</div>;
+  }
+  const documents = [];
   return (
-    <div>
-      <Header />
-    </div>
+    <main className='home-container'>
+      <Header className='sticky left-0 top-0'>
+        <div className='fext items-center gap-2 lg:gap-4'>
+          Notification
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+      {documents.length > 0 ? (
+        <div>
+
+        </div>
+      ):(
+        <div className='document-list-empty'>
+          <Image
+            src='/assets/icons/doc.svg'
+            alt='Document'
+            width={40}
+            height={40}
+            className='mx-auto'
+          />
+          <AddDocumentBtn
+            userId= {clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress}/>
+        </div>
+      )}
+    </main>
   )
 }
 
-export default page
+export default Home
