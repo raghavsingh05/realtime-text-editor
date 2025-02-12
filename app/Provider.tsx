dd0f89ca9132c65bf6b7c71737bbcc624634408f
raghavsingh05 <raghavsingh47336@gmail.com>
2025-02-12 12:13:54 +0530

    Add document user fetching functionality and integrate mention suggestions in Provider component

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -7,15 +7,26 @@ import {
     ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import Loader from "@/components/Loader";
-import { getClerkUsers } from "@/lib/actions/user.action";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.action";
+import { useUser } from "@clerk/nextjs";
 
 const Provider = ({ children }: { children: ReactNode }) => {
+    const { user : clerkUser} = useUser();
     return (
         <LiveblocksProvider 
         authEndpoint="/api/liveblocks-auth" 
         resolveUsers={async({userIds}) => {
             const users = await getClerkUsers({userIds});
             return users;
+            }}
+            resolveMentionSuggestions={ async ({ text, roomId})=>{
+                const roomUsers = await getDocumentUsers({
+                    roomId,
+                    currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+                    text,
+                })
+
+                return roomUsers
             }}
         >
                 <ClientSideSuspense fallback={<Loader />}>
