ecfa9a9ee4384adcd01e06874e155b3f8f5d8153
raghavsingh05 <raghavsingh47336@gmail.com>
2025-02-14 16:44:42 +0530

    Add Radix UI Label and Select components; implement Collaborator and UserTypeSelector components for document access management

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -3,7 +3,7 @@
 import { nanoid } from 'nanoid'
 import { liveblocks } from '../liveblocks';
 import { revalidatePath } from 'next/cache';
-import { parseStringify } from '../utils';
+import { getAccessType, parseStringify } from '../utils';
 import { redirect } from 'next/navigation';
 
 export const createDoucument = async({userId, email}:
@@ -22,7 +22,7 @@ export const createDoucument = async({userId, email}:
             const room = await liveblocks.createRoom(roomId, {
                 metadata,
                 usersAccesses,
-                defaultAccesses:['room:write']
+                defaultAccesses:[]
             });
 
             revalidatePath('/');
@@ -37,11 +37,11 @@ export const createDoucument = async({userId, email}:
 export const getDocument = async ({roomId, userId }:{roomId: string; userId:string}) =>{
     try {
         const room = await liveblocks.getRoom(roomId);
-        // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+        const hasAccess = Object.keys(room.usersAccesses).includes(userId);
 
-        // if (!hasAccess) {
-        //     throw new Error('You do not have access to this document');
-        // }
+        if (!hasAccess) {
+            throw new Error('You do not have access to this document');
+        }
         return parseStringify(room);
     } catch (error) {
         console.log("Error happened while fetching the room", error);
@@ -70,6 +70,60 @@ export const getDocuments = async (email:string) =>{
         return parseStringify(rooms);
     } catch (error) {
         console.log("Error happened while fetching the rooms", error);
+    }
+}
+
+export const updateDocumentAccess = async ({ roomId, email, userType, updatedBy}: ShareDocumentParams) =>{
+    try {
+        const usersAccesses: RoomAccesses = {
+          [email]: getAccessType(userType) as AccessType,
+        }
+    
+        const room = await liveblocks.updateRoom(roomId, { 
+          usersAccesses
+        })
+    
+        if(room) {
+        //   const notificationId = nanoid();
+    
+        //   await liveblocks.triggerInboxNotification({
+        //     userId: email,
+        //     kind: '$documentAccess',
+        //     subjectId: notificationId,
+        //     activityData: {
+        //       userType,
+        //       title: `You have been granted ${userType} access to the document by ${updatedBy.name}`,
+        //       updatedBy: updatedBy.name,
+        //       avatar: updatedBy.avatar,
+        //       email: updatedBy.email
+        //     },
+        //     roomId
+        //   })
+        }
+    
+        revalidatePath(`/documents/${roomId}`);
+        return parseStringify(room);
+      } catch (error) {
+        console.log(`Error happened while updating a room access: ${error}`);
+      }
+}
+
+export const removeCOllaborator = async ({ roomId, email}:{roomId: string, email:string} ) =>{
+    try {
+        const room = await liveblocks.getRoom(roomId)
+        if(room.metadata.email === email){
+            throw new Error('You cannot remove yourself from the document')
+        }
+
+        const updatedRoom = await liveblocks.updateRoom(roomId, {
+            usersAccesses: {
+                [email]: null
+            }
+        })
+        revalidatePath(`/documents/${roomId}`);
+        return parseStringify(updatedRoom);
+    } catch (error) {
+        console.log(`Error happenend while removing a collaborator: ${error}`)
     }
 }
 
